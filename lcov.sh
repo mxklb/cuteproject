0beab3b3a3b237d8375fd8745a4a5ef28f325fab
mxklb <dev@xamblak.de>
2017-10-06 01:11:06 +0200

    Refactored some scripts & opimized debian packaging #2

--- lcov.sh
+++ lcov.sh
@@ -10,7 +10,7 @@
 reportname="coverage"
 
 # Working directory
-folder="coverage/"
+folder="coverage"
 mkdir $folder
 
 # Prepare for lcov, remove moc_* files coverage
@@ -18,19 +18,19 @@ find . -name "moc_*.gcda" -delete
 find . -name "moc_*.gcno" -delete
 
 # A] Create a compressed lcov coverage report from . directory (ignore libs/extern)
-lcov --directory . --capture --output-file "$folder"app.info --no-external
-lcov --remove "$folder"app.info "libs/extern/*" -o "$folder"app.info
-genhtml --output-directory $folder "$folder"app.info
-tar czf "$reportname".tar.gz $folder
-mv "$reportname".tar.gz $folder
+lcov --directory . --capture --output-file $folder/app.info --no-external
+lcov --remove $folder/app.info "libs/extern/*" -o $folder/app.info
+genhtml --output-directory $folder $folder/app.info
+tar czf $reportname.tar.gz $folder
+mv $reportname.tar.gz $folder
 
 # B] Create a pdf from html
-outfile="$reportname.pdf"
+outfile=$reportname.pdf
 
 # Find all index.html and *.gcov.html files (see genhtml)
-indexFiles=$(find "$folder" -name index.html -printf "%d %p\n" | sort -n | perl -pe 's/^\d+\s//;')
-otherFiles=$(find "$folder" -name *.gcov.html | sort)
-files=$(echo -e $indexFiles $otherFiles $folder$outfile)
+indexFiles=$(find $folder -name index.html -printf "%d %p\n" | sort -n | perl -pe 's/^\d+\s//;')
+otherFiles=$(find $folder -name *.gcov.html | sort)
+files=$(echo -e $indexFiles $otherFiles $folder/$outfile)
 
 # Execute wkhtmltopdf.sh to wget wkhtmltox-*_linux-generic-amd64 binary
 chmod +x libs/extern/wkhtmltox.sh && libs/extern/wkhtmltox.sh
--- pkgs/debian.sh
+++ pkgs/debian.sh
@@ -4,55 +4,65 @@ scriptPath=`pwd`
 popd > /dev/null
 cd "$scriptPath"
 
+# Set package maintainer
+export LOGNAME=$USER
+
 # Get version info from file
 version=$(cat deb/app/version)
 majver=${version:0:3}
+pkgname="app"
 
 # Create debian folder ($PACKAGE-$VERSION, f.e myapp-4.2-1)
-mkdir deb/app-$version
-mkdir deb/app-$version/app
-mkdir deb/app-$version/libs
-mkdir deb/app-$version/img
+mkdir deb/$pkgname-$version
+mkdir deb/$pkgname-$version/app
+mkdir deb/$pkgname-$version/libs
+mkdir deb/$pkgname-$version/img
 
 # Copy plain sources files (qt/qmake .pro project folder)
-cp -r ../app/* deb/app-$version/app
-cp -r ../libs/* deb/app-$version/libs
-cp -r ../img/* deb/app-$version/img
-cp ../globals.pri deb/app-$version
-cp ../alltests.pri deb/app-$version
-cp ../cuteproject.pro deb/app-$version
+cp -r ../app/* deb/$pkgname-$version/app
+cp -r ../libs/* deb/$pkgname-$version/libs
+cp -r ../img/* deb/$pkgname-$version/img
+cp ../globals.pri deb/$pkgname-$version
+cp ../alltests.pri deb/$pkgname-$version
+cp ../cuteproject.pro deb/$pkgname-$version
 
 # Copy other files (some desktop config)
-cp deb/app/app.desktop deb/app-$version/cuteproject.desktop
+cp deb/app/app.desktop deb/$pkgname-$version/$pkgname.desktop
 
 # Go into build directory
-cd deb/app-$version
+cd deb/$pkgname-$version
 
 # Configure build without tests
-sed -i '1s/.*/catchTests=false/' globals.pri
+echo "catchTests=false" > globals.pri
 
 # Initialize debian configurations
 dh_make --createorig --yes --single --email dev@xamblak.de --copyright mit
 
 # Overwrite defaults with custom configs
-cp ../app/app.install debian/app-$majver.install
+cp ../app/app.install debian/$pkgname-$majver.install
 cp ../app/control debian
 cp ../app/menu debian
 cp ../app/rules debian
 cp ../app/dirs debian
 
 # Write source/package version info into the control file
-sed -i '2s/.*/Version='$majver'/' cuteproject.desktop
-sed -i '1s/.*/Source: app-'$majver'/' debian/control
-sed -i '11s/.*/Package: app-'$majver'/' debian/control
+sed -i '2s/.*/Version='$majver'/' $pkgname.desktop
+sed -i '1s/.*/Source: '$pkgname'-'$majver'/' debian/control
+sed -i '11s/.*/Package: '$pkgname'-'$majver'/' debian/control
 
 # Set proper rights
 #chmod -R -x+X *
-chmod -x debian/app-$majver.install
+chmod -x debian/$pkgname-$majver.install
 chmod -x debian/control
 chmod +x debian/rules
 chmod -x debian/menu
 chmod -x debian/dirs
+
+# Correct debian/copyright template
+sed -i '32,37d' debian/copyright
+sed -i '4,9d' debian/copyright
+sed -i '6d' debian/copyright
+sed -i '3s/.*/Source: <https:\/\/github.com\/mxklb\/cuteproject>/' debian/copyright
 
 # Build the package
 dpkg-buildpackage -b -rfakeroot -us -uc -tc
