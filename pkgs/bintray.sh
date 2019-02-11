80701c4e4a3b4be37ae36364c66875d92c1e0be8
mxklb <mxklb@protonmail.com>
2019-02-11 22:31:34 +0100

    Added automated bintray deployment for debain packages #15

--- pkgs/bintray.sh
+++ pkgs/bintray.sh
@@ -1,36 +1,57 @@
 #!/bin/bash
-# This script publishes artifacts to bintray
+# This script publishes artifacts from gitlab-ci to bintray.
+# Depending on the package type it pushes to different repos.
+# Supported package types are: debian & appimage
+# Debain upload example: ./bintray.sh debian
 
+pushd `dirname $0` > /dev/null
+scriptPath=`pwd`
+popd > /dev/null
+cd "$scriptPath"
+
+if [ -z "$1" ]; then
+  echo "Package type parameter missing!"
+  echo " Try: bintray.sh debian|appimage|.."
+  exit 0
+fi
+
+# Bintray setup
 USER="mxklb"
 COMPANY="mx-klb"
-API_KEY="$BINTRAY_API_KEY"
-VERSION="1.0-5"
-
-# Debian specifics
-OS="xenial"
 REPO="cuteproject"
-DISTRIBUTIONS="testing"
-COMPONENTS="main"
-ARCHITECTURES="amd64"
+API_KEY="$BINTRAY_API_KEY"
 
-DEBIAN_FILE_PATH="pkgs/deb"
-DEBIAN_FILE_NAME="cuteproject-1.0_5-1_amd64.deb"
-DEBIAN_FILE="$DEBIAN_FILE_PATH/$DEBIAN_FILE_NAME"
+# Upload debian artifacts to bintray
+if [ "$1" == "debian" ]; then
+  COMPONENTS="main"
+  ARCHITECTURES="amd64"
 
-PACKAGE="$REPO-$OS"
-TARGET="pool/c/$REPO-${VERSION}_$OS"
-TARGET_PATH="$TARGET/$DEBIAN_FILE_NAME"
+  # Detect ubuntu codename
+  OS=$(lsb_release -c --short)
 
-# Upload debian artifact to bintray
-curl -vT $DEBIAN_FILE -u${USER}:${API_KEY} "https://api.bintray.com/content/$COMPANY/$REPO/$PACKAGE/$VERSION/$TARGET_PATH;deb_distribution=$DISTRIBUTIONS;deb_component=$COMPONENTS;deb_architecture=$ARCHITECTURES;publish=1"
+  DISTRIBUTIONS="$OS"
+  DEBIAN_FILE=$(ls deb/*.deb)
+  DEBIAN_FILE_PATH=$(echo ${DEBIAN_FILE%/*})
+  DEBIAN_FILE_NAME=$(echo ${DEBIAN_FILE##*/})
 
-# Upload appImage artifacts to bintray
-#curl -T $APPIMAGE_FILE -u$USER:$API_KEY "https://api.bintray.com/content/$COMPANY/cuteproject/<YOUR_COOL_PACKAGE_NAME>/<VERSION_NAME>/<FILE_TARGET_PATH>"
+  PACKAGE="$REPO-$OS"
+  VERSION=$(bash ./version.sh)
+  TARGET="pool/c/$REPO-${VERSION}_$OS"
+  TARGET_PATH="$TARGET/$DEBIAN_FILE_NAME"
 
+  curl -T $DEBIAN_FILE -u${USER}:${API_KEY} "https://api.bintray.com/content/$COMPANY/$REPO/$PACKAGE/$VERSION/$TARGET_PATH;deb_distribution=$DISTRIBUTIONS;deb_component=$COMPONENTS;deb_architecture=$ARCHITECTURES;publish=1"
+# Upload appimage artifacts to bintray
+elif [ "$1" == "appimage" ]; then
+  echo "Actually .AppImage upload is not supported!"
+  # Upload appImage artifacts to bintray
+  #curl -T $APPIMAGE_FILE -u${USER}:${API_KEY} "https://api.bintray.com/content/$COMPANY/cuteproject/<YOUR_COOL_PACKAGE_NAME>/<VERSION_NAME>/<FILE_TARGET_PATH>"
 
-#Debain package upload:
-#curl -T <FILE.deb> -umxklb:<API_KEY> https://api.bintray.com/content/mx-klb/cuteproject/cuteproject-trusty/<VERSION_NAME>/<FILE_TARGET_PATH>;deb_distribution=<DISTRIBUTIONS>;deb_component=<COMPONENTS>;deb_architecture=<ARCHITECTURES>
-#curl -T <FILE.deb> -umxklb:<API_KEY> https://api.bintray.com/content/mx-klb/cuteproject/<YOUR_COOL_PACKAGE_NAME>/<VERSION_NAME>/<FILE_TARGET_PATH>;deb_distribution=<DISTRIBUTIONS>;deb_component=<COMPONENTS>;deb_architecture=<ARCHITECTURES>
+  #Debain package upload:
+  #curl -T <FILE.deb> -umxklb:<API_KEY> https://api.bintray.com/content/mx-klb/cuteproject/cuteproject-trusty/<VERSION_NAME>/<FILE_TARGET_PATH>;deb_distribution=<DISTRIBUTIONS>;deb_component=<COMPONENTS>;deb_architecture=<ARCHITECTURES>
+  #curl -T <FILE.deb> -umxklb:<API_KEY> https://api.bintray.com/content/mx-klb/cuteproject/<YOUR_COOL_PACKAGE_NAME>/<VERSION_NAME>/<FILE_TARGET_PATH>;deb_distribution=<DISTRIBUTIONS>;deb_component=<COMPONENTS>;deb_architecture=<ARCHITECTURES>
 
-#Any file upload:
-#curl -T <FILE.EXT> -umxklb:<API_KEY> https://api.bintray.com/content/mx-klb/cuteproject/<YOUR_COOL_PACKAGE_NAME>/<VERSION_NAME>/<FILE_TARGET_PATH>
+  #Any file upload:
+  #curl -T <FILE.EXT> -umxklb:<API_KEY> https://api.bintray.com/content/mx-klb/cuteproject/<YOUR_COOL_PACKAGE_NAME>/<VERSION_NAME>/<FILE_TARGET_PATH>
+else
+  echo "Unsupported package type parameter!"
+fi
