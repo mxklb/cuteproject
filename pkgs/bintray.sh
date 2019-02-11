f0636aee062956ad69c562589b19a06d2b793bc0
mxklb <mxklb@protonmail.com>
2019-02-12 00:49:26 +0100

    Optimized bintray deployment

--- pkgs/bintray.sh
+++ pkgs/bintray.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This script publishes artifacts from gitlab-ci to bintray.
 # Depending on the package type it pushes to different repos.
-# Supported package types are: debian & appimage
-# Debain upload example: ./bintray.sh debian KEY
+# Supported package types are: trusty, xenial & appimage
+# Debain upload example: ./bintray.sh trusty KEY
 
 pushd `dirname $0` > /dev/null
 scriptPath=`pwd`
@@ -22,13 +22,10 @@ REPO="cuteproject"
 API_KEY="$2"
 
 # Upload debian artifacts to bintray
-if [ "$1" == "debian" ]; then
+if [ "$1" == "trusty" ] || [ "$1" == "xenial" ] ; then
+  OS="$1"
   COMPONENTS="main"
   ARCHITECTURES="amd64"
-
-  # Detect ubuntu codename
-  UBUNTU_CODENAME=$(cat /etc/lsb-release | grep DISTRIB_CODENAME)
-  OS=$(echo ${UBUNTU_CODENAME##*=})
 
   DISTRIBUTIONS="$OS"
   DEBIAN_FILE=$(ls ../*.deb)
@@ -38,6 +35,8 @@ if [ "$1" == "debian" ]; then
   VERSION=$(bash ./version.sh)
   TARGET="pool/c/$REPO-${VERSION}_$OS"
   TARGET_PATH="$TARGET/$DEBIAN_FILE_NAME"
+
+  echo "$DISTRIBUTIONS: $TARGET_PATH $USER"
 
   curl -T $DEBIAN_FILE -u${USER}:${API_KEY} "https://api.bintray.com/content/$COMPANY/$REPO/$PACKAGE/$VERSION/$TARGET_PATH;deb_distribution=$DISTRIBUTIONS;deb_component=$COMPONENTS;deb_architecture=$ARCHITECTURES;publish=1"
 # Upload appimage artifacts to bintray
@@ -53,5 +52,5 @@ elif [ "$1" == "appimage" ]; then
   #Any file upload:
   #curl -T <FILE.EXT> -umxklb:<API_KEY> https://api.bintray.com/content/mx-klb/cuteproject/<YOUR_COOL_PACKAGE_NAME>/<VERSION_NAME>/<FILE_TARGET_PATH>
 else
-  echo "Unsupported package type parameter!"
+  echo "Unsupported package type parameter $1"
 fi
